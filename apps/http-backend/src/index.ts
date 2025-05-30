29077d22a85982df602849e6b47ec05bb8140dfa
Osmosiac111 <neevjain482@gmail.com>
2025-05-30 22:21:24 +0530

    add turbo.json in http and ws server

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken"
 
 const app = express()
 const PORT = 3001
+
+app.use(express.json())
+
+app.post("/signup", (req, res) => {
+    res.json({ msg: "signup" })
+})
+
+app.post("/signin", (req, res) => {
+    res.json({ msg: "signin" })
+})
+
+app.post("/room", (req, res) => {
+    res.json({ msg: "create-sign" })
+})
 
 app.listen(PORT)
